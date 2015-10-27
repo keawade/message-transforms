d568a1fca994c04d453d031587424802be5328de
Keith Wade <keawade@gmail.com>
2015-10-27 21:47:39 +0000

    Basic layout implemented

---
+++ transforms.js
@@ -0,0 +1,64 @@
+function buildContent(){
+    var options = ['Reverse','Leetspeak','Lolspeak','Igpay Atinlay'];
+
+    var wrapper = document.createElement('div');
+    wrapper.id = 'container';
+    
+    wrapper.appendChild(makeHeading('Message Transmogrifier'));
+    wrapper.appendChild(makeTextArea('Enter text to transmogrify!'));
+    wrapper.appendChild(makeButtonList(options));
+    document.body.appendChild(wrapper);
+}
+
+function makeHeading(string){
+    var headingElem = document.createElement('h1');
+    headingElem.textContent = string;
+    return headingElem;
+}
+
+function makeTextArea(string){
+    var textArea = document.createElement('textarea');
+    textArea.placeholder = string;
+    return textArea;
+}
+
+function makeButton(string){
+    var button = document.createElement('button');
+    button.textContent = string;
+    button.id = string.toLowerCase().replace(/\W/,'');
+    button.className = 'transmog-button';
+    button.addEventListener('click', transmog, false);
+    return button;
+}
+
+function makeButtonList(array){
+    var buttonWrap = document.createElement('div');
+    buttonWrap.className = 'buttonWrap';
+    for(var i = 0; i < array.length; i++){
+        buttonWrap.appendChild(makeButton(array[i]));
+    }
+    return buttonWrap;
+}
+
+function transmog(event){
+    
+}
+
+// String manipulations
+function reverse(string){
+    string.split('').reverse().join('');
+}
+
+function leet(string){
+    return string;
+}
+
+function lols(string){
+    return string;
+}
+
+function pigLatin(string){
+    
+}
+
+buildContent();
